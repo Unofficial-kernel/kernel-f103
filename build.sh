48ddab7f7ab2b20eb4329e33bbc671ba26a8c426
raymondmiracle <34572535+raymondmiracle@users.noreply.github.com>
2019-01-13 23:49:29 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 export ARCH=arm64
 export CROSS_COMPILE=~/Toolchain/bin/aarch64-linux-android-
 mkdir output
-make -C $(pwd) O=output rosy_defconfig
+make -C $(pwd) O=output X521_6753_defconfig
 make -j32 -C $(pwd) O=output
